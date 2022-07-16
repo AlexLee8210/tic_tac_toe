c296d20792da8b65c1e661cda65f4223fe322a93
Alexander Lee <alexlee8210@gmail.com>
2022-07-16 01:30:14 -0500

    Added a board

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,11 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 import './piece.dart';
+import './board.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-Widget _buildGrid() => GridView.extent(
-    maxCrossAxisExtent: 150,
-    padding: const EdgeInsets.all(4),
-    mainAxisSpacing: 4,
-    crossAxisSpacing: 4,
-    children: List.generate(
-      10,
-      (index) {
-        return Center(
-          child: Text(
-            'Item $index',
-          ),
-        );
-      },
-    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -41,21 +24,26 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: List.generate(
-                      6,
-                      (index) => Piece(
-                            pieceType: ((index + 1) / 2.0).round(),
-                            pieceColor: Colors.red,
-                          ))),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(
+                  6,
+                  (index) => Piece(
+                    pieceType: ((index + 1) / 2.0).round(),
+                    pieceColor: Colors.red,
+                  ),
+                ),
+              ),
+              Board(),
               Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: List.generate(
-                      6,
-                      (index) => Piece(
-                            pieceType: ((index + 1) / 2.0).round(),
-                            pieceColor: Colors.blue,
-                          ))),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(
+                  6,
+                  (index) => Piece(
+                    pieceType: ((index + 1) / 2.0).round(),
+                    pieceColor: Colors.blue,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
